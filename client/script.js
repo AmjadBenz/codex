11dc7b1bbdbeccb9721d3e45ae6aecaf742951bb
Mac <mac@MacBook-Pro-de-Mac.local>
2023-02-10 10:26:19 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5300", {
+  const response = await fetch("https://codex-chatgpt-4lgn.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
